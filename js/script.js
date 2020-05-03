190fed8cb0b2ea32768d3119a5acfb4e429f7aa4
tomasz wyrwa <wyrwa.t@gmail.com>
2020-05-03 21:40:31 +0200

    add mobile menu

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
 const cameraIcon = document.querySelector('.menu__item--image');
-
+const menuItem = document.querySelectorAll('.menu__item');
+const menuTitle = document.querySelector('.home__title');
+const socialMedia = document.querySelector('.home__social-media');
+const homeWrapper = document.querySelector('.home__wrapper');
 menuActive = () => {
-    const menuItem = document.querySelectorAll('.menu__item');
     menuItem.forEach((item) => {
         item.classList.toggle('menu__item--active');
     })
+    menuTitle.classList.toggle('home__title--inactive');
+    homeWrapper.classList.toggle('home__wrapper--menu-on');
+    socialMedia.classList.toggle('home__title--inactive');
 }
 cameraIcon.addEventListener('click', menuActive);
